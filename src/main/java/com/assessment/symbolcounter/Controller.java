dd63931e950f026527f2ad9817c382cf7d860f99
Softneer <vicpreston98@gmail.com>
2024-05-16 15:33:10 +0300

    Check range

--- src/main/java/com/assessment/symbolcounter/Controller.java
+++ src/main/java/com/assessment/symbolcounter/Controller.java
@@ -11,6 +11,9 @@ public class Controller {
     SymbolCounterService symbolCounterService;
     @GetMapping("/number-of-times/{n}")
     public String countSymbols(@PathVariable int n) {
+        if (n < 1 || n > 31) {
+            return "The number of clicks is out of range";
+        }
         int[] result = symbolCounterService.countSymbols(n);
         return result[0] +" "+ result[1];
     }
